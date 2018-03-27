6cc3b4723f6a99c70e4877b909d2a5b171fc62dd
fdmazzone <fdmazzone@gmail.com>
2018-03-27 17:50:27 -0300

    Otra vez

--- Mecanica Celeste/Estabilidad Soluciones Periodicas Sitnikov Generalizado/ProbInvCol.jl
+++ Mecanica Celeste/Estabilidad Soluciones Periodicas Sitnikov Generalizado/ProbInvCol.jl
@@ -1,11 +1,13 @@
 function ColinealInv(x)
   #x vector fila de posiciones
   l=size(x)[2]
-  D=ones(size(x'))*x
+  D=ones(l,1)*x
   C=D-D'
   A=((abs.(C)).^(-3)).*C
   for i=1:l
     A[i,i]=0
   end
-  return (A-eye(l))\x'
+  return A\(-x')
 end
+A = [ ColinealInv([-1 -x -x*y x*y x 1]) for x=0.1:.01:.99, y=0.1:.01:.99 ];
+B=[~any(k->k<0,l) for l in A]
