5d7a7691c5d090cfe02a6593b1009354df4483fb
fdmazzone <fdmazzone@gmail.com>
2018-03-27 00:12:23 -0300

    Solucion problema inverso CC colineal

--- Mecanica Celeste/Estabilidad Soluciones Periodicas Sitnikov Generalizado/ProbInvCol.jl
+++ Mecanica Celeste/Estabilidad Soluciones Periodicas Sitnikov Generalizado/ProbInvCol.jl
@@ -1,10 +1,11 @@
 function ColinealInv(x)
   #x vector fila de posiciones
-  l=size(x)
+  l=size(x)[2]
   D=ones(size(x'))*x
-  A=(D-D').^(-2)
+  C=D-D'
+  A=((abs.(C)).^(-3)).*C
   for i=1:l
     A[i,i]=0
-  end 
-  return A
+  end
+  return (A-eye(l))\x'
 end
